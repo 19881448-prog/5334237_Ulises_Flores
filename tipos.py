55363ea1da2fe2a8eaa26a2aa691d2ec987128b6
19881448-prog <19881448@clases.edu.sv>
2026-05-20 22:10:30 +0100

    Add files via upload

---
+++ tipos.py
@@ -0,0 +1,43 @@
+x,y,z = "piña", "uva", "fresa"
+print(x)
+print(y)
+print(z)
+print("")
+x=y=z = "naranja"
+print(x)
+print(y)
+print(z)
+print("")
+fruta = ["piña", "uva", "fresa", "limon"]
+x,y,z, a = fruta
+print(x)
+print(y)
+print(z)
+print(a)x,y,z = "piña", "uva", "fresa"
+print(x)
+print(y)
+print(z)
+print("")
+x=y=z = "naranja"
+print(x)
+print(y)
+print(z)
+print("")
+fruta = ["piña", "uva", "fresa", "limon"]
+x,y,z, a = fruta
+print(x)
+print(y)
+print(z)
+print(a)from operator import truediv
+
+x=4
+print(type(x))
+x = "Migo"
+print(x)
+print(type(x))
+x = 10.1
+print(type(x))
+x= True
+print(type(x))
+x=[1,2,3]
+print(type(x))
