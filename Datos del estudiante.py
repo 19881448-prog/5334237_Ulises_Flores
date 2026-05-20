a768599f33efe1c1d4a56b657074d3fa1d7aaff6
19881448-prog <19881448@clases.edu.sv>
2026-05-20 21:53:58 +0100

    Add files via upload

---
+++ Datos del estudiante.py
@@ -0,0 +1,24 @@
+nombre=input("ingrese el nombre:")
+edad=input("ingrese la edad:")
+nie=input("ingrese el NIE:")
+seccion=input("ingrese la secion")
+
+# crear un archivo
+archivo=open("datosdel estudiante.text","w")
+
+archivo.write=open("datos del estudiante/n")
+archivo.write=("Nombre:"+nombre+"/n")
+archivo.write=("edad:"+edad+
+"/n")
+archivo.write=("nie:"+ nie +"/n")
+archivo.write=("seccion:"+seccion+"/n")
+
+
+archivo.close()
+
+print("Archivo creado correctamnte")
+
+
+
+
+
